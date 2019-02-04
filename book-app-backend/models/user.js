738434676e8a0fd4f26171e0d56f8f8377a0d758
Surbabu Pukkala <surbabupukkala@nbnco.com.au>
2019-02-05 00:03:54 +1100

    Add Models

---
+++ book-app-backend/models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    name: String,
+    user_type: String
+});
+
+module.exports = mongoose.model('User', userSchema);
